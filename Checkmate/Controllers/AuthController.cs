18b301923b7a870057f4e819e37440ad06d0a231
Nicolas G <nicolas.gueb.dev@gmail.com>
2025-10-02 16:01:24 +0200

    ok

--- Checkmate/Controllers/AuthController.cs
+++ Checkmate/Controllers/AuthController.cs
@@ -2,24 +2,24 @@
 using Checkmate.Models;
 using Microsoft.AspNetCore.Mvc;
 
-//namespace Checkmate.Controllers
-//{
-//    public class AuthController(Chesscontext _db) : Controller
-//    {
-//        [HttpGet]
-//        public IActionResult Login()
-//        {
-//            return View();
-//        }
+namespace Checkmate.Controllers
+{
+    public class AuthController(Chesscontext _db) : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-//        [HttpPost]
-//        public IActionResult Login(LoginForm form)
-//        {
-//            User? user = _db.Users.FirstOrDefault(u => u.Pseudo == form.Username);
+        [HttpPost]
+        public IActionResult Login(LoginForm form)
+        {
+            User? user = _db.Users.FirstOrDefault(u => u.Pseudo == form.Username);
 
 
-//            TempData["error"] = "Invalid Credentials";
-//            return View();
-//        }
-//    }
-//}
+            TempData["error"] = "Invalid Credentials";
+            return View();
+        }
+    }
+}
